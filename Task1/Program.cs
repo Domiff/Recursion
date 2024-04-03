1ac394d9a739433dc774809f6a8a2b37abcdde84
Дмитрий Левыкин <dmlegasy@dmail.com>
2024-04-03 12:33:46 +0300

    Added Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿int PrintNumbers(int m, int n)
+{
+    int result = 0;
+
+    if (m == n)
+    {
+        Console.WriteLine(result + m);
+        return n;
+    }
+    if (m > n)
+    {
+        Console.WriteLine(m - result);
+        return m;
+    }
+    Console.WriteLine(result + m);
+    m++;
+    
+    PrintNumbers(m, n);
+    return result;
+}
+
+
+PrintNumbers(2, 4);
